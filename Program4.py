46061b1020fbe8d8c969575425803665be9a7929
Avneesh Gupta <agpt.2001@gmail.com>
2020-05-25 01:12:55 +0530

    Day1

---
+++ Program4.py
@@ -0,0 +1,7 @@
+cp=float(input("Enter Cost Price: "))
+sp=float(input("Enter Selling Price: "))
+profit=sp-cp
+sp1=cp+(profit*(105/100))
+print("----------OUTPUT----------")
+print("Profit Gained:",profit)
+print("Selling Price if Profit inc. by 5%:",sp1)
