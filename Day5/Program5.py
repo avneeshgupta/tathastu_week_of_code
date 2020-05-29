2e37c8a74769afd413ec1687e774ef2944a0297f
avneeshgupta <51831466+avneeshgupta@users.noreply.github.com>
2020-05-29 16:42:21 +0530

    Day 5

---
+++ Day5/Program5.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def sort_list(list1,num1):
+    c=0
+    for x in range(num1):
+        if list1[x]%2==0:
+            for y in range(x+1,num1):
+                if list1[y]%2!=0:
+                    list1[x],list1[y]=list1[y],list1[x]
+                    c+=1
+                    break
+        else:
+            continue
+    list1=sorted(list1[:c],reverse=True)+sorted(list1[c:])
+    return list1
+
+num1=int(input("Enter total number of integer elements: "))
+list1=[]
+for x in range(num1):
+    list1.append(int(input("Enter integer element "+str(x+1)+": ")))
+print("Updated list:",sort_list(list1,num1))
